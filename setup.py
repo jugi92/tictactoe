27a0de3cb8eae0878c3c1e7d6888e51df91944bd
jugi92 <juliangimbel@gmail.com>
2021-05-08 09:23:28 +0200

    package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tictactoe',
+      version='0.1',
+      description='Great Game',
+      url='https://github.com/jugi92/tictactoe',
+      author='jugi',
+      license='MIT',
+      packages=['tictactoe'],
+      zip_safe=False)
